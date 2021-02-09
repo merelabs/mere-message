b852b86850d9be8dd199f24e8f5a022cf800361c
Bhuiyan Mohammad Iklash <bhuiyan.mohammad.iklash@gmail.com>
2021-02-09 14:01:20 +0600

    Fix signal and dtor

--- samples/client/client.h
+++ samples/client/client.h
@@ -3,19 +3,18 @@
 
 #include "mere/message/client.h"
 
-#include <QObject>
-
 class Client : public QObject
 {
     Q_OBJECT
 public:
+    ~Client();
     explicit Client(QObject *parent = nullptr);
 
 signals:
 
 public slots:
     void pong(const int &pid);
-    void message(const QString &message);
+    void message(const std::string &message);
     void message(const Mere::Message::Message &message);
 
     void seen(const pid_t &pid, const mid_t &id);
